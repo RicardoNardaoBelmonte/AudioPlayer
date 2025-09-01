7ae49d93de318523d6d053e2bd570f368379e9af
Ricardo Nardao Belmonte <nardaoricardo@gmail.com>
2025-09-01 10:51:37 -0300

    Add routes for the pages using  react-router-dom

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,16 @@
+
 import { Home } from "./componentes/Home"
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
-    <main>
-      <Home/>
-    </main>
+      {/* Adicionei Routes sobre os elementos então toda vez q for criar outra pagina ou sla coloca */}
+      {/* <Route/> e dentor chama os parametros path que voce cria a rota pra aql pagna e element para voce chamar qual elemento(pagina) quer mostrar nesta rota*/}
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+      </Routes>
     </>
   )
 }
