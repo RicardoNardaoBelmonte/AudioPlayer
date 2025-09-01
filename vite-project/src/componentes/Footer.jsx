d0d75c8420713317f606e9c9d21656681070e5df
Ricardo Nardao Belmonte <nardaoricardo@gmail.com>
2025-09-01 10:52:59 -0300

    Created the footer component to be reused on others elements

---
+++ vite-project/src/componentes/Footer.jsx
@@ -0,0 +1,41 @@
+import proximoLeft from '../assets/home/header/proximoLeft.png';
+import proximoRight from '../assets/home/header/proximoRight.png';
+import playButton from '../assets/home/header/playButton.png';
+import loop from '../assets/home/header/loop.png';
+import capaMusica from '../assets/home/header/capaMusica.png';
+import soundVolume from '../assets/home/header/soundVolume.png';
+
+export function Footer() {
+
+    return(
+        <footer className='mx-15 bg-background pt-5 rounded-lg'>
+            <div className="flex flex-col">
+                <div className="flex items-center gap-50 mx-5">
+                    <div className="flex gap-5">
+                        <img className='w-15 h-15' src={capaMusica} alt="" />
+                        <div className='flex flex-col'>
+                            <span className='text-white text-base'>Get Lucky feat. Pharrell Williams and Nile Rodgers</span>
+                            <span className='text-base text-gray-400'>Daft Punk</span>
+                        </div>
+                    </div>
+
+                    <div className="flex gap-7 items-center">
+                        <img className='w-5 h-5' src={loop} alt="" />
+                        <img className='w-6 h-6' src={proximoLeft} alt="" />
+                        <img className='w-6 h-6' src={playButton} alt="" />
+                        <img className='w-6 h-6' src={proximoRight} alt="" />
+                    </div>
+
+                    <div className='flex gap-20'>
+                        <span className='text-sm text-white ml-20'>2:18 / 3:21</span>
+                        <img className='w-6 h-6' src={soundVolume} alt="" />
+                    </div>
+                </div>
+
+                <div id='barraTempo' className='h-[5px] w-full bg-white relative rounded mt-2'>
+                    <div className='h-[5px] bg-primary w-230 absolute rounded z-10'></div>
+                </div>
+            </div>
+        </footer>
+    )
+} 
