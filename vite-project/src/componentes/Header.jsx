dcbb38984bab4a7472b8fcc22a7d0dd50a494258
Ricardo Nardao Belmonte <nardaoricardo@gmail.com>
2025-09-02 09:13:44 -0300

    Adding the dropdown-menu

--- vite-project/src/componentes/Header.jsx
+++ vite-project/src/componentes/Header.jsx
@@ -1,14 +1,26 @@
+import React from 'react';
 import hamburguerButton from '../assets/home/header/hamburguerButton.png';
 import logo from '../assets/home/header/logo.png';
 
 export function Header() {
-    //Aqui criei o Header que pode ser chamado em outros componentes oq È bom j· q vamos reutilizar ele
-    //Ent„o so importa e chama o componente que nem eu fiz na Home.jsx que ele ja aparece mesma coisa com o footer
+
+    const [active, setActive] = React.useState(false);
+    //Aqui criei o Header que pode ser chamado em outros componentes oq √© bom j√° q vamos reutilizar ele
+    //Ent√£o so importa e chama o componente que nem eu fiz na Home.jsx que ele ja aparece mesma coisa com o footer
     return(
         <header className='' >
             <nav className='h-18 flex items-center w-full'>
                 <div className='mx-10 w-7 h-7'>
-                    <button className=''><img src={hamburguerButton} alt="" /></button>
+                    <button className='relative cursor-pointer' onClick={() => setActive(!active)}><img className='h-5 w-5' src={hamburguerButton} alt="" /></button>
+                    {active && (
+                        <ul className='absolute left-0 mt-1 ml-5 w-40 rounded-2xl bg-background p-1 border-black border rounded z-50'>
+                            <li className='hover:bg-gray-400 text-white px-3 rounded'><a href="">Arquivo</a></li>
+                            <li className='hover:bg-gray-400 text-white px-3 rounded'><a href="">Editar</a></li>
+                            <li className='hover:bg-gray-400 text-white px-3 rounded'><a href="">Exibir</a></li>
+                            <li className='hover:bg-gray-400 text-white px-3 rounded'><a href="">Configura√ß√µes</a></li>
+                            <li className='hover:bg-gray-400 text-white px-3 rounded'><a href="">Ajuda</a></li>
+                        </ul>
+                    )}
                 </div>
 
                 <div className='flex items-center gap-2 ml-140'>
