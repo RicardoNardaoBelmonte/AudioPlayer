eef203604149d07e8dfecd160de4c6fe7f77ab06
Ricardo Nardao Belmonte <nardaoricardo@gmail.com>
2025-09-02 09:40:07 -0300

    Adding the favorite star in the footer

--- vite-project/src/componentes/Footer.jsx
+++ vite-project/src/componentes/Footer.jsx
@@ -4,18 +4,22 @@ import playButton from '../assets/home/header/playButton.png';
 import loop from '../assets/home/header/loop.png';
 import capaMusica from '../assets/home/header/capaMusica.png';
 import soundVolume from '../assets/home/header/soundVolume.png';
+import estrela from '../assets/home/header/estrela.png';
 
 export function Footer() {
 
     return(
         <footer className='mx-15 bg-background pt-5 rounded-lg'>
             <div className="flex flex-col">
-                <div className="flex items-center gap-50 mx-5">
-                    <div className="flex gap-5">
+                <div className="flex items-center gap-45 mx-5">
+                    <div className="flex gap-4 items-center">
                         <img className='w-15 h-15' src={capaMusica} alt="" />
                         <div className='flex flex-col'>
                             <span className='text-white text-base'>Get Lucky feat. Pharrell Williams and Nile Rodgers</span>
                             <span className='text-base text-gray-400'>Daft Punk</span>
+                        </div>
+                        <div>
+                            <img className='' src={estrela} alt="" />
                         </div>
                     </div>
 
