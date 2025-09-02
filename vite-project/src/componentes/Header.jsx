31f6eee025647b3327715b2e5946c5a842cba057
Ricardo Nardao Belmonte <nardaoricardo@gmail.com>
2025-09-02 09:29:09 -0300

    Styling the dropdown-menu with animation and diferent color in hover

--- vite-project/src/componentes/Header.jsx
+++ vite-project/src/componentes/Header.jsx
@@ -12,15 +12,14 @@ export function Header() {
             <nav className='h-18 flex items-center w-full'>
                 <div className='mx-10 w-7 h-7'>
                     <button className='relative cursor-pointer' onClick={() => setActive(!active)}><img className='h-5 w-5' src={hamburguerButton} alt="" /></button>
-                    {active && (
-                        <ul className='absolute left-0 mt-1 ml-5 w-40 rounded-2xl bg-background p-1 border-black border rounded z-50'>
-                            <li className='hover:bg-gray-400 text-white px-3 rounded'><a href="">Arquivo</a></li>
-                            <li className='hover:bg-gray-400 text-white px-3 rounded'><a href="">Editar</a></li>
-                            <li className='hover:bg-gray-400 text-white px-3 rounded'><a href="">Exibir</a></li>
-                            <li className='hover:bg-gray-400 text-white px-3 rounded'><a href="">Configurações</a></li>
-                            <li className='hover:bg-gray-400 text-white px-3 rounded'><a href="">Ajuda</a></li>
+                        <ul className={`absolute left-0 mt-1 ml-5 w-40 rounded-2xl bg-background p-1 border-black border rounded z-50 transform transition-all duration-300 ease-in-out origin-top
+                        ${active ? "opacity-100 scale-100 visible" : "opacity-0 scale-y-0 invisible"}`}>
+                            <li className='hover:bg-[#262626] text-white px-3 rounded'><a href="">Arquivo</a></li>
+                            <li className='hover:bg-[#262626] text-white px-3 rounded'><a href="">Editar</a></li>
+                            <li className='hover:bg-[#262626] text-white px-3 rounded'><a href="">Exibir</a></li>
+                            <li className='hover:bg-[#262626] text-white px-3 rounded'><a href="">Configurações</a></li>
+                            <li className='hover:bg-[#262626] text-white px-3 rounded'><a href="">Ajuda</a></li>
                         </ul>
-                    )}
                 </div>
 
                 <div className='flex items-center gap-2 ml-140'>
