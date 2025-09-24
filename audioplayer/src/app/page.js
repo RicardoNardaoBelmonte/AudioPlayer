6755726b302e77259f405134ef6e4350884af2d3
Ricardo Nardao Belmonte <nardaoricardo@gmail.com>
2025-09-24 07:58:18 -0300

    privede the queryClient for tanstack-query

--- audioplayer/src/app/page.js
+++ audioplayer/src/app/page.js
@@ -1,9 +1,16 @@
-import Home from './home/page.js';
+"use client";
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Home from "./home/page.js";
 
 export default function Pages() {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
-        <main>
-          <Home/>
-        </main>
-    );
+    <QueryClientProvider client={queryClient}>
+      <main>
+        <Home />
+      </main>
+    </QueryClientProvider>
+  );
 }
