acd1d9ebf43924d30ad79b2631e94a655a19f3ee
Ricardo Nardao Belmonte <nardaoricardo@gmail.com>
2025-09-25 13:47:01 -0300

    provide duration time in the response and remove what we dont need, and also remove the client_id and client_secret from our SpotfyWorkspace

--- audioplayer/src/app/api/fetchMusicas.js
+++ audioplayer/src/app/api/fetchMusicas.js
@@ -1,23 +1,13 @@
-<<<<<<< HEAD
 "use server"
 const client_id = process.env.CLIENT_ID
 const client_secret = process.env.CLIENT_SECRET
-=======
-// Coloque suas credenciais do Spotify
-const CLIENT_ID = 'b36f46a6fb1b487b816c59738f7d29d1';
-const CLIENT_SECRET = 'a9db02fa2c5346b2added151baf25834';
->>>>>>> 8127fdc37f0992b23fd9e263b75fbec2af526e80
 
  async function getAccessToken() {
     const response = await fetch('https://accounts.spotify.com/api/token', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
-<<<<<<< HEAD
         'Authorization': 'Basic ' + Buffer.from(client_id + ':' + client_secret).toString('base64'),
-=======
-        'Authorization': 'Basic ' + Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64'),
->>>>>>> 8127fdc37f0992b23fd9e263b75fbec2af526e80
       },
       body: 'grant_type=client_credentials',
     });
@@ -36,13 +26,12 @@ const CLIENT_SECRET = 'a9db02fa2c5346b2added151baf25834';
     });
   
     const data = await response.json();
-    
+
     return data.tracks.items.map(track => ({
       nome: track.name,
       artista: track.artists.map(a => a.name).join(', '),
-      album: track.album.name,
-      thumb: track.album.images[0]?.url || null, // primeira imagem do �lbum
-      url: track.external_urls.spotify
+      thumb: track.album.images[0]?.url || null, 
+      duracao: track.duration_ms,
     }));
   }
   
