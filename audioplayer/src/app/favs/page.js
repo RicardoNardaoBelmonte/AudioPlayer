b26f240f3de1ff620f5652fc6b437f8940bc8eea
Ricardo Nardao Belmonte <nardaoricardo@gmail.com>
2025-10-03 10:14:41 -0300

    add responsivness in page favs

--- audioplayer/src/app/favs/page.js
+++ audioplayer/src/app/favs/page.js
@@ -1,59 +1,87 @@
 "use client"
 import {Header} from '../componentes/Header.js';
 import Image from 'next/image';
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient  } from '@tanstack/react-query';
 import Corpo from '../componentes/Corpo.js';
 import { Footer } from '../componentes/Footer.js';
+import delet from '../../../public/assets/home/header/delet.png';
 
 export default function Favs(){
 
-        const {data: MusicasDb = [], isLoading, isError} = useQuery({
-            queryKey: ['musicas'],
-            queryFn: async () => {
+    const queryClient = useQueryClient();
 
-                const token = localStorage.getItem('token');
+    const {data: MusicasDb = [], isLoading, isError} = useQuery({
+        queryKey: ['musicas'],
+        queryFn: async () => {
 
-                if (!token) throw new Error("Você precisa estar Logado");
+            const token = localStorage.getItem('token');
 
-                const res = await fetch('/api/musicas', {
-                    headers: { Authorization: `Bearer ${token}`}
-                });
+            if (!token) throw new Error("Você precisa estar Logado");
 
-                if(!res.ok) throw new Error("Erro ao buscar músicas do usuário");
+            const res = await fetch('/api/musicas', {
+                headers: { Authorization: `Bearer ${token}`}
+            });
 
-                const musicas = await res.json();
-                return musicas.musicas;
-            }
-        })
+            if(!res.ok) throw new Error("Erro ao buscar músicas do usuário");
+
+            const musicas = await res.json();
+            return musicas.musicas;
+        }
+    })
+
+    const deletarMusica = useMutation({
+        mutationFn: async (data) => {
+            const token = localStorage.getItem('token');
+
+            const res = await fetch("/api/musicas", {
+                method: "DELETE",
+                headers: {Authorization: `Bearer ${token}`, "Content-Type": "application/json"},
+                body: JSON.stringify(data)
+            });
+
+            const json = await res.json();
+            if(!res.ok) throw new Error(json.error || "Erro ao remover música dos favoritos")
+            return json;
+        }, 
+        onSuccess: () =>  {
+            alert("Música removida dos favoritos"),
+            queryClient.invalidateQueries({ queryKey: ['musicas'] });
+        },
+        onError: () =>  alert("Erro ao remover música dos favoritos")
+    }) 
 
     return(
         <>
             <Header/>
             <Corpo>
-                <div className='grid grid-cols-12 items-center px-4 py-3 text-white border-b border-white text-base'>
+                <div className='grid grid-cols-12 items-center px-4 py-3 text-white border-b border-white text-base 2xl:text-xl'>
                     <div className="col-span-6">Título</div>
-                    <div className="col-span-4">Artista</div>
-                    <div className="col-span-2 text-right">⏱</div>
+                    <div className="col-span-4">Arista</div>
+                    <div className="col-span-2 text-right">Deletar</div>
                 </div>
 
                 {isLoading ? <span className='text-white flex text-center items-center justify-center mt-30 text-xl'>Carregando Musicas</span> : ''} 
                 {isError ? <span className='text-white flex text-center items-center justify-center mt-30 text-xl'>Você precisa estar logado</span> : ''}
 
-                <ul className='max-h-96 overflow-y-auto scrollbar-hide'>
+                <ul className='max-h-80 xl:max-h-96 2xl:max-h-[500] overflow-y-auto scrollbar-hide'>
                     {MusicasDb.map((musica, index) => (
-                        <li key={index} className='grid grid-cols-12 items-center p-5 cursor-pointer hover:bg-borderGray mt-2 rounded-xl'>
-                            <div className='col-span-5'>
-                                <div className='flex tems-center gap-5 items-center'>
-                                    <Image className='rounded' src={musica.thumb} width={50} height={50} alt='thumb da musica'/>
-                                    <span className='text-white w-60 overflow-hidden truncate'>{musica.titulo}</span>
+                        <li key={index} >
+                            <div  className='grid grid-cols-12 items-center p-5 cursor-pointer hover:bg-borderGray mt-2 rounded-xl w-full'>
+                                <div className='col-span-5'>
+                                    <div className='flex tems-center items-center gap-5'>
+                                        <Image className='rounded' src={musica.thumb} width={50} height={50} alt='thumb da musica'/>
+                                        <div className='flex flex-col text-textPrimary'>
+                                            <span className='text-white w-60 text-start overflow-hidden truncate text-xs xl:text-base'>{musica.titulo}</span>
+                                            <span>{musica.duracao}</span>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className='col-span-6 w-70 overflow-hidden truncate text-textPrimary'>
-                                <span className='text-center'>{musica.artista}</span>
-                                <audio></audio>
-                            </div>
-                            <div className='col-span-1 text-textPrimary text-end'>
-                                <span>{musica.duracao}</span>
+                                <div className='col-span-6 w-70 overflow-hidden truncate text-textPrimary text-start'>
+                                    <span className='text-center text-xs xl:text-base'>{musica.artista}</span>
+                                </div>
+                                <div className='col-span-1 text-textPrimary text-end'>
+                                    <button onClick={() => deletarMusica.mutate(musica.id) } className='cursor-pointer'><Image className='rounded w-5 h-5' src={delet} alt='deletar musica'/></button>
+                                </div> 
                             </div>
                         </li>
                     ))}
