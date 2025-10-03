cfa88a2ca05db33cb6a924b751cd08fef0da3173
Ricardo Nardao Belmonte <nardaoricardo@gmail.com>
2025-10-03 10:14:12 -0300

    add responsivness to corpo component

--- audioplayer/src/app/componentes/Corpo.js
+++ audioplayer/src/app/componentes/Corpo.js
@@ -20,19 +20,19 @@ export default function Corpo({children, activeLink, setActiveLink}){
     ]
 
     return(
-        <section className='mx-15 mt-15 mb-15 h-[60vh] flex gap-20'>
+        <section className='md:mx-5 xl:mx-15 mt-35 md:mt-30 mb-15 h-[60vh] flex gap-20 min-w-230'>
                 {/* Criei a section com margens e tamanho especifico para ficar bom o gap é entre a aside e a div */}
-                <aside className='h-full w-50 bg-background p-1 rounded-lg'>
+                <aside className='h-full min-w-40 md:min-w-45 2xl:min-w-50 bg-background p-1 rounded-lg'>
                     <nav className='h-full w-full'>
                         <ul className='flex flex-col gap-5 pt-10'>
                             {linkNav.map((item) => (
-                                <li key={item.name} className='w-full h-10 pl-5 hover:bg-[#262626] transition-colors rounded'><Link href={item.path} className='flex gap-5 items-center w-full h-full'><Image src={currentPath === item.path ? item.iconAtivo : item.icon } className='w-4 h-4' alt="" /><span className='text-gray-400'>{item.name}</span></Link></li>
+                                <li key={item.name} className='w-full h-10 pl-5 hover:bg-[#262626] transition-colors rounded'><Link href={item.path} className='flex gap-5 items-center w-full h-full'><Image src={currentPath === item.path ? item.iconAtivo : item.icon } className='w-4 h-4' alt="" /><span className='text-gray-400 xs:text-xs text-base md:text-xl 2xl:text-2xl'>{item.name}</span></Link></li>
                             ))}
                         </ul>
                     </nav>
                 </aside>
                 {/* Criei a aside a div lateral para por os link fiz um map onde eu meio que passo por todos os itens da lista de objetos e para cada item eu chamo o respectivo nome e afins */}
-                <div className='h-full w-full bg-background p-5 rounded-lg'>
+                <div className='h-full w-full bg-background p-5 rounded-lg '>
                     {children}
                 </div>
                 {/* Criei a div que fica no centro Escrvendo os textos e colocando as cores basicamente tudo utilizando html e css aq so q com nome de classes do tailwind para não precisar por no index.css global */}
